10c60098a1ddc81092e7b777d2fa118487bedbd6
Anderson Passos <anderson.passos@yahoo.com.br>
2025-04-14 18:52:58 -0300

    estrutura inicial do projeto

---
+++ src/test/java/com/motoclub/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoclub.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
