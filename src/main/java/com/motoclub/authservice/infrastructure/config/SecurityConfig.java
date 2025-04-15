e32e4946eb0a83bfa38f5eae54f111e4378e2e14
Anderson Passos <anderson.passos@yahoo.com.br>
2025-04-14 21:43:46 -0300

    feat: adiciona configuração do SecurityFilterChain e liberação do acesso ao console H2

--- src/main/java/com/motoclub/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/motoclub/authservice/infrastructure/config/SecurityConfig.java
@@ -2,10 +2,25 @@ package com.motoclub.authservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
