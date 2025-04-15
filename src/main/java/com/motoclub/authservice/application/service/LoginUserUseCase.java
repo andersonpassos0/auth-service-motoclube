7dbec170afc992b2dd10a9cae57fa9b4b3be9473
Anderson Passos <anderson.passos@yahoo.com.br>
2025-04-14 21:18:32 -0300

    feat: adiciona bean de BCryptPasswordEncoder para codificação de senha

--- src/main/java/com/motoclub/authservice/application/service/LoginUserUseCase.java
+++ src/main/java/com/motoclub/authservice/application/service/LoginUserUseCase.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class LoginuserUseCase {
+public class LoginUserUseCase {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
