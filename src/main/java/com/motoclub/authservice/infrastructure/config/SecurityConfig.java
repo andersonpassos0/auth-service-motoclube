d51b437236580855a65d7fa2fae55f3db7650bf2
Anderson Passos <anderson.passos@yahoo.com.br>
2025-04-16 18:17:31 -0300

    feat: ajustes

--- src/main/java/com/motoclub/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/motoclub/authservice/infrastructure/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.motoclub.authservice.infrastructure.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
